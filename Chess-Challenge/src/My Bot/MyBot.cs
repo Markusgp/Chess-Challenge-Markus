23b135125223ea25d31e631aef8fb3a0166388da
Markusgp <mgrandp@outlook.com>
2023-07-22 00:21:02 +0200

    Added mobility heuristic, still no checkmates

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,12 +8,14 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        //Console.WriteLine("New eval: " + evaluateBoard(board));
-        return Minimax(board, 5, float.NegativeInfinity, float.PositiveInfinity, false, new Move()).Item2;
+        Console.WriteLine("New eval: " + evaluateBoard(board));
+        return Minimax(board, 5, float.NegativeInfinity, float.PositiveInfinity, new Move()).Item2;
     }
 
     public float evaluateBoard(Board board)
     {
+        float eval = 0;
+
         var pieceValues = new Dictionary<PieceType, int> {
             { PieceType.None, 0 },
             { PieceType.King, 900 },
@@ -24,19 +26,21 @@ public class MyBot : IChessBot
             { PieceType.Pawn, 10 }
         };
 
-        float eval = 0;
         foreach (var pList in board.GetAllPieceLists())
         {
             foreach (Piece p in pList)
             {
+                //Evaluation heuristic
                 float value = pieceValues[p.PieceType];
-                eval += p.IsWhite ? value : -value;
+                eval += p.IsWhite ? value : -value; //Value of pieces
+                eval += p.IsWhite ? board.GetLegalMoves().Length : - board.GetLegalMoves().Length; //Amount of legal moves (mobility)
+                
             }
         }
         return eval;
     }
 
-    public (float, Move) Minimax(Board board, int depth, float alpha, float beta, bool isMaximizer, Move bestMove)
+    public (float, Move) Minimax(Board board, int depth, float alpha, float beta, Move bestMove)
     {
         if (depth == 0 || board.GetLegalMoves().Length == 0)
         {
@@ -45,13 +49,13 @@ public class MyBot : IChessBot
 
         Move[] legalMoves = board.GetLegalMoves();
 
-        if (isMaximizer)
+        if (board.IsWhiteToMove)
         {
             float bestValue = float.NegativeInfinity;
             foreach (Move m in legalMoves)
             {
                 board.MakeMove(m);
-                (float value, _) = Minimax(board, depth - 1, alpha, beta, false, bestMove);
+                (float value, _) = Minimax(board, depth - 1, alpha, beta, bestMove);
                 board.UndoMove(m); // Undo the move after evaluating
 
                 if (value > bestValue)
@@ -74,7 +78,7 @@ public class MyBot : IChessBot
             foreach (Move m in legalMoves)
             {
                 board.MakeMove(m);
-                (float value, _) = Minimax(board, depth - 1, alpha, beta, true, bestMove);
+                (float value, _) = Minimax(board, depth - 1, alpha, beta, bestMove);
                 board.UndoMove(m); // Undo the move after evaluating
 
                 if (value < bestValue)
