58ba79a6d1bd2f98fd2d9105b5a2f4926ab1c485
Markusgp <mgrandp@outlook.com>
2023-07-23 14:32:56 +0200

    Updated with comments

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,12 +6,26 @@ using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
+    /// <summary>
+    /// <para> Submission by Markus Grand Petersen </para>
+    /// The main entry point for the bot to think about the move.
+    /// The bot uses the Minimax algorithm with Alpha-Beta pruning.
+    /// </summary>
     public Move Think(Board board, Timer timer)
     {
-        Console.WriteLine("New eval: " + evaluateBoard(board));
+        //Default opening move as white
+        if(board.GetFenString() == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" && board.IsWhiteToMove) {
+            return new Move("d2d4", board);
+        }
+        Console.WriteLine("Eval: "+evaluateBoard(board));
+
+        //White is the maximizer (goal is a positive score)
+        //Black is the minimizer (goal is a negative score)
         bool isMaximizing = board.IsWhiteToMove ? true : false;
-        return Minimax(board, 6, isMaximizing, float.NegativeInfinity, float.PositiveInfinity).Item2;
+        return Minimax(board, 5, isMaximizing, float.NegativeInfinity, float.PositiveInfinity).Item2;
     }
+
+    // Dictionary to assign values to each piece type.
     private readonly Dictionary<PieceType, int> pieceValues = new Dictionary<PieceType, int> {
         { PieceType.None, 0 },
         { PieceType.King, 900 },
@@ -22,33 +36,53 @@ public class MyBot : IChessBot
         { PieceType.Pawn, 10 }
     };
 
+    /// <summary>
+    /// Evaluates the board position based on the piece values and their mobility.
+    /// Also includes checks for draws, checks and checkmates.
+    /// </summary>
     public float evaluateBoard(Board board)
     {
         float eval = 0;
 
-        foreach (var pList in board.GetAllPieceLists())
+        foreach (PieceList pList in board.GetAllPieceLists())
         {
             foreach (Piece p in pList)
             {
-                // Base value of piece
+                // Base value of piece heuristic
                 float value = pieceValues[p.PieceType];
+
+                // // Mobility bonus
+                value += 0.05f * board.GetLegalMoves().Length;
+
+                //Sum of board eval score, white is positive, black is negative.
                 eval += p.IsWhite ? value : -value;
             }
         }
 
+        // King safety
+        if (board.IsInCheck())
+        {
+            eval += board.IsWhiteToMove ? -3 : 3;
+        }
+
+        // Checkmate and draw conditions
         if (board.IsInCheckmate())
         {
             eval = board.IsWhiteToMove ? float.NegativeInfinity : float.PositiveInfinity;
         }
-        else if (board.IsDraw())
+        if (board.IsDraw())
         {
-            eval = 0; // A draw should have an evaluation of 0
+            eval = 0;
         }
 
         return eval;
     }
 
-
+    /// <summary>
+    /// Minimax function with Alpha-Beta pruning.
+    /// This function runs recursively until it reaches the given depth or finds a terminal state (Checkmate or Draw).
+    /// It returns the evaluation of the board and the best move to reach that evaluation.
+    /// </summary>
     public (float, Move) Minimax(Board board, int depth, bool isMaximizing, float alpha, float beta)
     {
         if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
@@ -57,7 +91,7 @@ public class MyBot : IChessBot
         }
 
         Move[] moves = board.GetLegalMoves();
-
+        //moves.OrderByDescending(m => m.IsCapture).ToArray();
         Move bestMove = new Move();
 
         if (isMaximizing)
@@ -67,15 +101,18 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(m);
                 (float eval, Move _) = Minimax(board, depth - 1, false, alpha, beta);
-                board.UndoMove(m);
+                // Checking if the evaluation of this move is better than the current maxEval
                 if (eval > maxEval)
                 {
                     maxEval = eval;
                     bestMove = m;
                 }
+                board.UndoMove(m);
+                // Alpha-Beta pruning
                 alpha = Math.Max(alpha, maxEval);
-                if (beta <= alpha)
+                if (beta <= alpha) {
                     break;
+                }
             }
             return (maxEval, bestMove);
         }
@@ -86,15 +123,18 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(m);
                 (float eval, Move _) = Minimax(board, depth - 1, true, alpha, beta);
-                board.UndoMove(m);
+                // Checking if the evaluation of this move is better than the current minEval
                 if (eval < minEval)
                 {
                     minEval = eval;
                     bestMove = m;
                 }
+                board.UndoMove(m);
+                // Alpha-Beta pruning
                 beta = Math.Min(beta, minEval);
-                if (beta <= alpha)
+                if (beta <= alpha) {
                     break;
+                }
             }
             return (minEval, bestMove);
         }
